26cbe9a849ec1ef27340e57cfb0ba0eb574bcca3
EmilyKamleh <49740500+EmilyKamleh@users.noreply.github.com>
2021-04-12 22:48:35 -0500

    Started Health Script

---
+++ Assets/Scripts/Player/Health.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+using Bot.States;
+using Mirror;
+using Player;
+using UnityEngine;
+using UnityEngine.AI;
+
+namespace Health
+{
+    public delegate void Notify();
+
+    public class Health
+    {
+        private float health;
+        public event Action<float> OnHealthChanged;
+
+        public void Start()
+        {
+            Console.WriteLine("Process Started!");
+            // some code here..
+            OnHealthChanged();
+        }
+
+        protected virtual void OnHealthChanged() //protected virtual method
+        {
+            //if ProcessCompleted is not null then call delegate
+            HealthChanged?.Invoke();
+        }
+    }
+}
